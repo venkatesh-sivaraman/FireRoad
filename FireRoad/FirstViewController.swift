8fbb6745c82dbb28d2f50f72274cb8a9b63f8744
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2017-05-02 16:22:26 -0400

    Initial Commit

---
+++ FireRoad/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FireRoad
+//
+//  Created by Venkatesh Sivaraman on 5/2/17.
+//  Copyright © 2017 Base 12 Innovations. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
