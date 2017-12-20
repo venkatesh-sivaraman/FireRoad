958e505ceca985790bd84abd0657b72aa5111cce
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2017-12-20 12:28:44 -0500

    Better color choices (manual); added a color checker script for this purpose

--- FireRoad/CourseManager.swift
+++ FireRoad/CourseManager.swift
@@ -52,56 +52,35 @@ class CourseManager: NSObject {
         "MAS", "SCM", "STS", "SWE", "SP"
     ]
     static let colorMapping: [String: UIColor] = {
-        let baseColors: [(h: CGFloat, s: CGFloat, v: CGFloat)] = [
-            (0.4194, 0.78, 0.76),
-            (0.625, 0.66, 0.78),
-            (0.0, 0.70, 0.71),
-            (0.7722, 0.70, 0.72),
-            (0.575, 0.78, 0.98)
+        let baseValues: [(s: CGFloat, v: CGFloat)] = [
+            (0.7, 0.87),
+            (0.52, 0.71),
+            (0.88, 0.71)
         ]
-        var ret: [String: UIColor] = [:]
-        // Grouped into categories in which each member should be a different color
-        let colorGroups = [
-            ["1", "2", "16", "EC", "EM"],
-            ["3", "5", "10", "22", "SCM"],
-            ["4", "11", "12", "STS", "IDS"],
-            ["6", "CSB", "7", "HST", "20"],
-            ["14", "15", "17", "24", "9"],
-            ["21A", "21W", "21L", "WGS", "21H"],
-            ["21G", "21M", "CMS", "MAS", "21"],
-            ["8", "18", "CC", "ES", "SP"],
-            ["SWE"]
+        let directive: [String: (h: CGFloat, base: Int)] = [
+            "1": (0.0, 0), "2": (20.0, 0),
+            "3": (225.0, 0), "4": (128.0, 1),
+            "5": (162.0, 0), "6": (219.0, 0),
+            "7": (218.0, 2), "8": (267.0, 2),
+            "9": (276.0, 2), "10": (0.0, 2),
+            "11": (342.0, 1), "12": (125.0, 0),
+            "14": (30.0, 0), "15": (3.0, 1),
+            "16": (197.0, 0), "17": (315.0, 0),
+            "18": (236.0, 1), "20": (135.0, 2),
+            "21": (130.0, 2), "21A": (138.0, 2),
+            "21W": (146.0, 2), "CMS": (154.0, 2),
+            "21G": (162.0, 2), "21H": (170.0, 2),
+            "21L": (178.0, 2), "21M": (186.0, 2),
+            "WGS": (194.0, 2), "22": (0.0, 1),
+            "24": (260.0, 1), "CC": (115.0, 0),
+            "CSB": (197.0, 2), "EC": (100.0, 1),
+            "EM": (225.0, 1), "ES": (242.0, 1),
+            "HST": (218.0, 1), "IDS": (150.0, 1),
+            "MAS": (122.0, 2), "SCM": (138.0, 1),
+            "STS": (270.0, 0), "SWE": (13.0, 2),
+            "SP": (240.0, 0)
         ]
-        let saturationIntervals = 4, brightnessIntervals = 2
-        let saturationIncrement: CGFloat = 0.1, brightnessIncrement: CGFloat = 0.2
-        let maxSat = saturationIncrement * CGFloat(saturationIntervals / 2),
-        minSat = saturationIncrement * CGFloat(-saturationIntervals / 2)
-        let maxBright = brightnessIncrement, minBright = CGFloat(0.0)
-        
-        for (i, group) in colorGroups.enumerated() {
-            let satInc = saturationIncrement * CGFloat((i % saturationIntervals) - saturationIntervals / 2)
-            let brightInc = brightnessIncrement * CGFloat(i % brightnessIntervals)
-            for (j, course) in group.enumerated() {
-                var (h, s, v) = baseColors[j]
-                if s + satInc <= 0.3 {
-                    s += maxSat - satInc
-                } else if s + satInc >= 0.9 {
-                    s += minSat - satInc
-                } else {
-                    s += satInc
-                }
-                
-                if v + brightInc <= 0.5 {
-                    v += maxBright - brightInc
-                } else if v + brightInc >= 1.0 {
-                    v += minBright - brightInc
-                } else {
-                    v += brightInc
-                }
-
-                ret[course] = UIColor(hue: h, saturation: s, brightness: v, alpha: 1.0)
-            }
-        }
+        var ret = directive.mapValues({ UIColor(hue: $0.h / 360.0, saturation: baseValues[$0.base].s, brightness: baseValues[$0.base].v, alpha: 1.0) })
         let saturation = CGFloat(0.7)
         let brightness = CGFloat(0.87)
         ret["GIR"] = UIColor(hue: 0.05, saturation: saturation * 0.75, brightness: brightness, alpha: 1.0)
